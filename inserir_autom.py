5089a7b1022b8da26c3d5bc2ee3cc26c8f62acd0
JuanPBM1 <juanpatrickyt@gmail.com>
2024-12-03 12:40:59 -0300

    Consertando o caminho onde os arquivos são salvos

--- inserir_autom.py
+++ inserir_autom.py
@@ -8,16 +8,19 @@ print(f"Por favor certifique-se que o excel está fechado antes de inserir qualq
 print(f"Os arquivos csv e excel devem estar na pasta \"arquivos\"")
 
 caminho_csv = input("Insira o nome do arquivo csv (SEM A EXTENSÃO)")
+print(f"{caminho_csv}")
 caminho_excel = input("Insira o nome do arquivo excel (COM A EXTENSÃO DO ARQUIVO ex: arquivo.xlsx)")
 
 data = pd.read_csv(f"arquivos/{caminho_csv}.csv")
+print(data)
 arquivo = xl.load_workbook(f"arquivos/{caminho_excel}")
 backup = arquivo
 aba_atual = arquivo.active
+extensao_arquivo = Path(caminho_excel).suffix
 
-indice_inicial = input("Insira o número da linha do excel em que os dados devem começar a ser inseridos\n\n") # a linha do arquivo excel que deverá ser inserida
-num_colunas = input("Insira o número de colunas que deverão ser inseridas no arquivo excel") # o número de colunas que deverão ser inseridas no arquivo excel
-num_linhas = input("Insira o número de linhas que deverão ser inseridas no arquivo excel") # o número de linhas que deverão ser inseridas no arquivo excel
+indice_inicial = int(input("Insira o número da linha do excel em que os dados devem começar a ser inseridos\n\n")) # a linha do arquivo excel que deverá ser inserida
+num_colunas = int(input("Insira o número de colunas que deverão ser inseridas no arquivo excel")) # o número de colunas que deverão ser inseridas no arquivo excel
+num_linhas = int(input("Insira o número de linhas que deverão ser inseridas no arquivo excel")) # o número de linhas que deverão ser inseridas no arquivo excel
 
 #inserindo os valores no arquivo excel
 linha_dataframe = 0
@@ -28,6 +31,6 @@ for i in range(indice_inicial, indice_inicial+num_linhas): # vai do índice inic
 
 data_e_hora = datetime.now()
 data_hora_formatado = data_e_hora.strftime("%Y-%m-%d  %H-%M-%S")
-arquivo.save("Employees.xlsx")
-backup.save(f"backup{data_hora_formatado}.")
+arquivo.save("arquivos/Employees.xlsx")
+backup.save(f"backups/backup{data_hora_formatado}.{extensao_arquivo}")
         
